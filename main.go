51392f3edfdd83a917de77596baae07603f468a4
陈大人 <chensiyuan@pushplus.plus>
2023-08-03 19:57:08 +0800

    改成cookie

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
@@ -86,15 +85,16 @@ func main() {
 	// Initialize session store
 
 	var store sessions.Store
-	if os.Getenv("REDIS_CONN_STRING") == "" {
-		store = cookie.NewStore([]byte(common.SessionSecret))
-	} else {
-		store, _ = redis.NewStore(10, "tcp", common.RDB.Options().Addr, common.RDB.Options().Password)
-		store.Options(sessions.Options{
-			//7天过期
-			MaxAge: int(3600 * 24 * 7),
-		})
-	}
+	store = cookie.NewStore([]byte(common.SessionSecret))
+	// if os.Getenv("REDIS_CONN_STRING") == "" {
+	// 	store = cookie.NewStore([]byte(common.SessionSecret))
+	// } else {
+	// 	store, _ = redis.NewStore(10, "tcp", common.RDB.Options().Addr, common.RDB.Options().Password, []byte(common.SessionSecret))
+	// 	store.Options(sessions.Options{
+	// 		//7天过期
+	// 		MaxAge: int(3600 * 24 * 7),
+	// 	})
+	// }
 	server.Use(sessions.Sessions("session", store))
 
 	router.SetRouter(server, buildFS, indexPage)
