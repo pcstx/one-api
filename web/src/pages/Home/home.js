83f360b38f8912ef705bf6c0ae2e031147342d4e
陈大人 <chensiyuan@pushplus.plus>
2023-08-05 14:28:29 +0800

    首页样式修改

--- web/src/pages/Home/home.js
+++ web/src/pages/Home/home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Card, Grid, Header, Segment } from 'semantic-ui-react';
-import { API, showError, showNotice, timestamp2string } from '../../helpers';
+import { Card, Grid, Header, Segment, Container,Button } from 'semantic-ui-react';
+import { API, showError, showNotice } from '../../helpers';
 import { StatusContext } from '../../context/Status';
 import { marked } from 'marked';
 
@@ -30,10 +30,20 @@ const NewHome = () => {
   return (
     <>
       {
-         <Segment attached textAlign='center'>
-            <Header as='h3'>破壳AI</Header>
-                <div>测试破壳</div>
-          </Segment>
+            <Container textAlign="center" style={{margin:"100px 0 100px 0",width:"755px"}}>
+                <p style={{textAlign:'left',lineHeight:'30px'}}>
+                    <strong style={{fontSize:'46px'}}>破壳AI</strong>
+                </p>
+                <p style={{textAlign:'left',lineHeight:'30px'}}>
+                    <strong style={{fontSize:'46px'}}>生成式AI应用开发工具平台</strong>
+                </p>
+                <Container  style={{textAlign:'left',fontSize:'18px',lineHeight:'30px'}}>
+                整合AI的能力，将其应用范围拓展到更多领域，使AI技术普惠于广大人群。教育、政策制定和提高可访问性等措施将推动AI的全面发展，为社会带来积极的变革和发展机遇。
+                </Container>
+                <div>
+                    <Button class="ui button" size="big" color="blue" style={{margin:'60px',backgroundColor:'#5680ff'}}>开始使用</Button>
+                </div>
+            </Container>
       }
 
     </>
