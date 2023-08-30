dcf5a1756756472a7cd4fbc7e9a65ddd366e8f9b
陈大人 <chensiyuan@pushplus.plus>
2023-08-30 19:47:45 +0800

    更新版本

--- controller/relay-xunfei.go
+++ controller/relay-xunfei.go
@@ -177,38 +177,6 @@ func buildXunfeiAuthUrl(hostUrl string, apiKey, apiSecret string) string {
 	return callUrl
 }
 
-// 创建鉴权url  apikey 即 hmac username
-func assembleAuthUrl(hosturl string, apiKey, apiSecret string) string {
-	ul, err := url.Parse(hosturl)
-	if err != nil {
-		fmt.Println(err)
-	}
-	//签名时间
-	date := time.Now().UTC().Format(time.RFC1123)
-	//date = "Tue, 28 May 2019 09:10:42 MST"
-	//参与签名的字段 host ,date, request-line
-	signString := []string{"host: " + ul.Host, "date: " + date, "GET " + ul.Path + " HTTP/1.1"}
-	//拼接签名字符串
-	sgin := strings.Join(signString, "\n")
-	//签名结果
-	sha := HmacWithShaTobase64("hmac-sha256", sgin, apiSecret)
-	// fmt.Println(sha)
-	//构建请求参数 此时不需要urlencoding
-	authUrl := fmt.Sprintf("hmac username=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"", apiKey,
-		"hmac-sha256", "host date request-line", sha)
-	fmt.Println("authUrl:" + authUrl)
-	//将请求参数使用base64编码
-	authorization := base64.StdEncoding.EncodeToString([]byte(authUrl))
-
-	v := url.Values{}
-	v.Add("host", ul.Host)
-	v.Add("date", date)
-	v.Add("authorization", authorization)
-	//将编码后的字符串url encode后添加到url后面
-	callurl := hosturl + "?" + v.Encode()
-	return callurl
-}
-
 func xunfeiStreamHandler(c *gin.Context, textRequest GeneralOpenAIRequest, appId string, apiSecret string, apiKey string) (*OpenAIErrorWithStatusCode, *Usage) {
 	var usage Usage
 	query := c.Request.URL.Query()
@@ -228,19 +196,11 @@ func xunfeiStreamHandler(c *gin.Context, textRequest GeneralOpenAIRequest, appId
 	d := websocket.Dialer{
 		HandshakeTimeout: 5 * time.Second,
 	}
-	hostUrl := "wss://spark-api.xf-yun.com/v1.1/chat"
-	url := assembleAuthUrl(hostUrl, apiKey, apiSecret)
-	conn, resp, err := d.Dial(url, nil)
-	if err != nil {
-		fmt.Println("请求异常")
-		return errorWrapper(err, "dial_failed", http.StatusInternalServerError), nil
-	} else if resp.StatusCode != 101 {
+	conn, resp, err := d.Dial(buildXunfeiAuthUrl(hostUrl, apiKey, apiSecret), nil)
+	if err != nil || resp.StatusCode != 101 {
 		return errorWrapper(err, "dial_failed", http.StatusInternalServerError), nil
 	}
-	// if err != nil || resp.StatusCode != 101 {
-	// 	return errorWrapper(err, "dial_failed", http.StatusInternalServerError), nil
-	// }
-	data := requestOpenAI2Xunfei(textRequest, appId)
+	data := requestOpenAI2Xunfei(textRequest, appId, domain)
 	err = conn.WriteJSON(data)
 	if err != nil {
 		return errorWrapper(err, "write_json_failed", http.StatusInternalServerError), nil
@@ -328,11 +288,4 @@ func xunfeiHandler(c *gin.Context, resp *http.Response) (*OpenAIErrorWithStatusC
 	c.Writer.WriteHeader(resp.StatusCode)
 	_, err = c.Writer.Write(jsonResponse)
 	return nil, &fullTextResponse.Usage
-}
-
-func HmacWithShaTobase64(algorithm, data, key string) string {
-	mac := hmac.New(sha256.New, []byte(key))
-	mac.Write([]byte(data))
-	encodeData := mac.Sum(nil)
-	return base64.StdEncoding.EncodeToString(encodeData)
 }
