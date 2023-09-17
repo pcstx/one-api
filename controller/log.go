e3ce04edc9bf39bfcdfaf5aa1c7d64955923f1e5
JustSong <songquanpeng@foxmail.com>
2023-09-17 17:09:56 +0800

    feat: able to delete logs now (close #486)

--- controller/log.go
+++ controller/log.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"net/http"
 	"one-api/common"
 	"one-api/model"
 	"strconv"
@@ -21,7 +22,7 @@ func GetAllLogs(c *gin.Context) {
 	modelName := c.Query("model_name")
 	page, err := model.GetAllLogsPageList(logType, startTimestamp, endTimestamp, modelName, username, tokenName, p+1, int64(common.ItemsPerPage))
 	if err != nil {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
 			"message": err.Error(),
 		})
@@ -36,6 +37,7 @@ func GetAllLogs(c *gin.Context) {
 		"currentPage": page.CurrentPage,
 		"pageSize":    page.PageSize,
 	})
+	return
 }
 
 func GetUserLogs(c *gin.Context) {
@@ -52,7 +54,7 @@ func GetUserLogs(c *gin.Context) {
 	modelName := c.Query("model_name")
 	page, err := model.GetUserLogsPageList(userId, logType, startTimestamp, endTimestamp, modelName, tokenName, p+1, int64(common.ItemsPerPage))
 	if err != nil {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
 			"message": err.Error(),
 		})
@@ -67,23 +69,25 @@ func GetUserLogs(c *gin.Context) {
 		"currentPage": page.CurrentPage,
 		"pageSize":    page.PageSize,
 	})
+	return
 }
 
 func SearchAllLogs(c *gin.Context) {
 	keyword := c.Query("keyword")
 	logs, err := model.SearchAllLogs(keyword)
 	if err != nil {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
 			"message": err.Error(),
 		})
 		return
 	}
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
 		"data":    logs,
 	})
+	return
 }
 
 func SearchUserLogs(c *gin.Context) {
@@ -91,17 +95,18 @@ func SearchUserLogs(c *gin.Context) {
 	userId := c.GetInt("id")
 	logs, err := model.SearchUserLogs(userId, keyword)
 	if err != nil {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"success": false,
 			"message": err.Error(),
 		})
 		return
 	}
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
 		"data":    logs,
 	})
+	return
 }
 
 func GetLogsStat(c *gin.Context) {
@@ -113,7 +118,7 @@ func GetLogsStat(c *gin.Context) {
 	modelName := c.Query("model_name")
 	quotaNum := model.SumUsedQuota(logType, startTimestamp, endTimestamp, modelName, username, tokenName)
 	//tokenNum := model.SumUsedToken(logType, startTimestamp, endTimestamp, modelName, username, "")
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
 		"data": gin.H{
@@ -121,6 +126,7 @@ func GetLogsStat(c *gin.Context) {
 			//"token": tokenNum,
 		},
 	})
+	return
 }
 
 func GetLogsSelfStat(c *gin.Context) {
@@ -132,7 +138,7 @@ func GetLogsSelfStat(c *gin.Context) {
 	modelName := c.Query("model_name")
 	quotaNum := model.SumUsedQuota(logType, startTimestamp, endTimestamp, modelName, username, tokenName)
 	//tokenNum := model.SumUsedToken(logType, startTimestamp, endTimestamp, modelName, username, tokenName)
-	c.JSON(200, gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"success": true,
 		"message": "",
 		"data": gin.H{
@@ -140,4 +146,30 @@ func GetLogsSelfStat(c *gin.Context) {
 			//"token": tokenNum,
 		},
 	})
+	return
+}
+
+func DeleteHistoryLogs(c *gin.Context) {
+	targetTimestamp, _ := strconv.ParseInt(c.Query("target_timestamp"), 10, 64)
+	if targetTimestamp == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": "target timestamp is required",
+		})
+		return
+	}
+	count, err := model.DeleteOldLog(targetTimestamp)
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"success": false,
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "",
+		"data":    count,
+	})
+	return
 }
